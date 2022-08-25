aed708133cd8fe248d0909b30f7e895592060446
[l30l] <[847816474@qq.com]>
2022-08-25 12:58:29 +0800

    toast plugin finished

---
+++ src/components/common/toast/Toast.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="toast" v-if="isShow">
+    {{ message }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Toast',
+  data() {
+    return {
+      message: '',
+      isShow: false
+    }
+  },
+  methods: {
+    showMessage(msg, delay = 1000) {
+      this.isShow = true
+      this.message = msg
+      setTimeout(() => {
+        this.isShow = false
+        this.message = ''
+      }, delay)
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.toast {
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  padding: 10px 15px;
+  background: rgba(0, 0, 0, 0.7);
+  color: #fff;
+}
+</style>
